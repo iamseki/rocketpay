91356be5b0162b71e21319ca65698f282f2ae1b7
iamseki <chrisyuri_19@hotmail.com>
2021-02-24 19:22:04 -0300

    refactor: remove unnecessary Rocketpay.Numbers alias

--- lib/rocketpay_web/controllers/users_controller.ex
+++ lib/rocketpay_web/controllers/users_controller.ex
@@ -1,8 +1,6 @@
 defmodule RocketpayWeb.UsersController do
   use RocketpayWeb, :controller
 
-  alias Rocketpay.Numbers
-
   alias Rocketpay.User
 
   def create(conn, params) do
@@ -15,5 +13,12 @@ defmodule RocketpayWeb.UsersController do
     conn
     |> put_status(:created)
     |> render("create.json", user: user)
+  end
+
+  defp handle_response({:error, result}, conn) do
+    conn
+    |> put_status(:bad_request)
+    |> put_view(RocketpayWeb.ErrorView)
+    |> render("400.json", result: result)
   end
 end
