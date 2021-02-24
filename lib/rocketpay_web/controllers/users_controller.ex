6b5772c4b4fe54e8b258afa991c7185fd00299fd
iamseki <chrisyuri_19@hotmail.com>
2021-02-23 21:39:22 -0300

    feat: add create user into database resource

---
+++ lib/rocketpay_web/controllers/users_controller.ex
@@ -0,0 +1,19 @@
+defmodule RocketpayWeb.UsersController do
+  use RocketpayWeb, :controller
+
+  alias Rocketpay.Numbers
+
+  alias Rocketpay.User
+
+  def create(conn, params) do
+    params
+    |> Rocketpay.create_user()
+    |> handle_response(conn)
+  end
+
+  defp handle_response({:ok, %User{} = user}, conn) do
+    conn
+    |> put_status(:created)
+    |> render("create.json", user: user)
+  end
+end
